a50d5045629294358a8ff15821dcef4562c41ba0
Heman Gandhi <hemangandhi@gmail.com>
2019-08-19 17:42:59 -0400

    fix tests

--- tests/lex_parse_test.py
+++ tests/lex_parse_test.py
@@ -5,6 +5,6 @@ import pytest
 def test_basic_tokens():
     assert list(tok.tokenize("(+ 1 1)")) == ['(', '+', '1', '1', ')']
     assert list(tok.tokenize("(+ 121 1 (* 2 3))")) == ['(', '+', '121', '1', '(', '*', '2', '3', ')', ')']
-    assert list(tok.tokenize("(+ 1 1)?F:int")) == ['(', '+', '1', '1', ')', '?F', ':int']
-    assert list(tok.tokenize("(+ 1 1) ?F :int")) == ['(', '+', '1', '1', ')', '?F', ':int']
+    assert list(tok.tokenize("(+ 1 1)?F:int")) == ['(', '+', '1', '1', ')', '?', 'F', ':', 'int']
+    assert list(tok.tokenize("(+ 1 1) ?F :int")) == ['(', '+', '1', '1', ')', '?', 'F', ':', 'int']
 
