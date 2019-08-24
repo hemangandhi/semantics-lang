35b0791d770acdabcc75bd4b5db723a13887a50a
Heman Gandhi <hemangandhi@gmail.com>
2019-08-24 18:11:37 -0400

    adds type parsing?no_sem:commit

--- tests/lex_parse_test.py
+++ tests/lex_parse_test.py
@@ -10,20 +10,31 @@ def test_basic_tokens():
     assert list(tok.tokenize("(+ 1 1) ?F :int")) == ['(', '+', '1', '1', ')', '?', 'F', ':', 'int']
     assert list(tok.tokenize("(+ (* 1 1 1) 1)")) == ['(', '+', '(', '*', '1', '1', '1', ')', '1', ')']
 
-def test_eval():
-    class PlusTimesCtx:
-        def literal(self, val):
-            if not val.isdigit():
-                if val in ['*', '+']:
-                    return val
-                raise ValueError("Nan")
-            return int(val)
-        def call(self, fn, args):
-            if fn not in ['+', '*']:
-                raise ValueError("Not a function")
-            return reduce(lambda x, y: {'+': x + y, '*': x * y}[fn], args, {'+': 0, '*': 1}[fn])
+class PlusTimesCtx:
+    def literal(self, val):
+        if not val.isdigit():
+            if val in ['*', '+']:
+                return val
+            raise ValueError("Nan")
+        return int(val)
+    def call(self, fn, args):
+        if fn not in ['+', '*']:
+            raise ValueError("Not a function")
+        return reduce(lambda x, y: {'+': x + y, '*': x * y}[fn], args, {'+': 0, '*': 1}[fn])
+    def get_semantics(self, name):
+        return name
+    def get_type(self, semantics, name, *generic_args):
+        return name
+    def validate_type(self, l, semantics, typ):
+        return (typ == 'int' and l not in ['+', '*']) or (typ == 'fn' and l in ['+', '*'])
 
+def test_eval():
     pt = PlusTimesCtx()
     assert tok.evaluate(list(tok.tokenize("(+ 1 1)")), pt)[0] == 2
     tm = list(tok.tokenize("(+ (* 1 1 1) 1)"))
     assert tok.evaluate(tm, pt)[0] == 2
+
+def test_types():
+    pt = PlusTimesCtx()
+    assert tok.evaluate(list(tok.tokenize("(+ 1 1)?F:int")), pt)[0] == 2
+    assert tok.evaluate(list(tok.tokenize("(+?F:(fn ?F:int ?F:int) 1 1)?F:int")), pt)[0] == 2
--- tokenizer.py
+++ tokenizer.py
@@ -30,13 +30,36 @@ class AbstractContext:
         pass
     def get_semantics(self, name):
         pass
-    def get_type(self, semantics, name):
+    def get_type(self, semantics, name, *generic_args):
         pass
     def validate_type(self, literal, semantics, type):
         pass
 
 def parse_type(tokens, context, index):
-    return 0, 0
+    """ return semantics, types, index """
+    if tokens[index] != '?':
+        raise ValueError("? prefix missing for ?semantics:type")
+    if index + 1 >= len(tokens):
+        raise ValueError("Expected semantics after ?")
+    semantics = context.get_semantics(tokens[index + 1])
+    if index + 2 >= len(tokens) or tokens[index + 2] != ':':
+        raise ValueError("Need : to delimit semantics and type")
+    if index + 3 >= len(tokens):
+        raise ValueError("Expected type after :")
+    if tokens[index + 3] == '(':
+        if index + 4 >= len(tokens):
+            raise ValueError("Expected type after (")
+        base_type = tokens[index + 4]
+        index += 5
+        args = []
+        while index < len(tokens) and tokens[index]:
+            sem, typ, index = parse_type(tokens, context, index)
+            args.append((sem, typ))
+        if tokens >= len(tokens) and tokens[index] != ')':
+            raise ValueError("Expected )")
+        return semantics, context.get_type(semantics, base_type, *args), index + 1
+    else:
+        return semantics, context.get_type(semantics, tokens[index + 3], []), index + 4
 
 def evaluate(tokens, context, index = 0):
     if index >= len(tokens):
@@ -56,7 +79,8 @@ def evaluate(tokens, context, index = 0):
         evaled = context.literal(t)
     if index + 1 < len(tokens) and tokens[index + 1] == '?':
         semantics, typ, index = parse_type(tokens, context, index + 1)
-        if context.validate_type(evaled, semantics, typ):
-            return evaled, index
+        if not context.validate_type(evaled, semantics, typ):
+            raise ValueError("type error in the code")
+        return evaled, index
     else:
         return evaled, index + 1
